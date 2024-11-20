e67c55486e10b43e0d81bfc536191609c52b7003
Acer <Acer@DESKTOP-02V6NEC>
2024-11-20 14:07:19 +0530

    initial commit

---
+++ Water Billing and Records/Models/Bill.cs
@@ -0,0 +1,12 @@
+﻿namespace Water_Billing_and_Records.Models
+{
+    public class Bill
+    {
+        public int BillId { get; set; }
+        public int CustomerId { get; set; }
+        public double Amount { get; set; } // Amount for the bill
+        public DateTime BillingDate { get; set; }
+        public bool IsPaid { get; set; } // Payment status
+        public virtual Customer Customer { get; set; }
+    }
+}
---
+++ Water Billing and Records/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Water_Billing_and_Records.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public double TotalWaterUsage { get; set; } // Total water usage in cubic meters
+    }
+}
